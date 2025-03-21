de1081013b710d6a059302d8417953d8034c9c8f
Z1ro <142613704+Z1rota@users.noreply.github.com>
2025-03-21 18:52:23 +0300

    Add files via upload

---
+++ Prog 1-2 sem/src/main/java/org/example/managers/RunManager.java
@@ -0,0 +1,44 @@
+package org.example.managers;
+
+import org.example.utility.UserScanner;
+
+import java.util.NoSuchElementException;
+import java.util.Scanner;
+
+/**
+ * Класс для управления выполнением команд.
+ * Обеспечивает чтение команд от пользователя и их выполнение.
+ */
+public class RunManager {
+    /**
+     * Менеджер команд, который используется для выполнения команд.
+     */
+    private final CommandManager commandManager;
+
+    /**
+     * Конструктор класса RunManager.
+     *
+     * @param commandManager менеджер команд, который будет использоваться для выполнения команд
+     */
+    public RunManager(CommandManager commandManager) {
+        this.commandManager = commandManager;
+    }
+
+    /**
+     * Основной метод для запуска цикла выполнения команд.
+     * Читает команды от пользователя и передает их на выполнение в CommandManager.
+     */
+    public void run() {
+        try {
+            Scanner scanner = UserScanner.getUserScanner(); // Сканер для чтения ввода пользователя
+            while (true) {
+                System.out.println("Для выведения списка комманд введите-help\nВведите команду:");
+                String input = scanner.nextLine().trim() + " "; // Чтение ввода пользователя
+                String[] command = input.split(" ", 2); // Разделение ввода на команду и аргументы
+                commandManager.execute(command[0], command[1]); // Выполнение команды
+            }
+        } catch (NoSuchElementException e) {
+            System.exit(0);
+        }
+    }
+}
